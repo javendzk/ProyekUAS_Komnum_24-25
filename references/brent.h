c64ae3a67f7bbcdff37f92ceeba0f644739b3d21
javendzk <javana.dz@gmail.com>
2025-05-31 11:29:13 +0700

    ADD: Kode referensi brents method (.c & .h)

---
+++ references/brent.h
@@ -0,0 +1,16 @@
+// Sumber; https://people.math.sc.edu/Burkardt/c_src/brent/brent.html
+
+
+double glomin ( double a, double b, double c, double m, double machep, 
+  double e, double t, double f ( double x ), double *x );
+double local_min ( double a, double b, double eps, double t, 
+  double f ( double x ), double *x );
+double local_min_rc ( double *a, double *b, int *status, double value );
+double r8_epsilon ( );
+double r8_max ( double x, double y );
+double r8_sign ( double x );
+void timestamp ( );
+double zero ( double a, double b, double machep, double t, 
+  double f ( double x ) );
+void zero_rc ( double a, double b, double t, double *arg, int *status, 
+  double value );
